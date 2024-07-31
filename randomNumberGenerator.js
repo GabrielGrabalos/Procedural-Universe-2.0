2ea9503ff33a32684ed52a2830429e613cb908ad
Gabriel Grabalos <bielgraba@gmail.com>
2024-07-31 18:16:35 -0300

    same seed for every pixel

--- randomNumberGenerator.js
+++ randomNumberGenerator.js
@@ -12,11 +12,12 @@ class RandomNumberGenerator {
     }
 
     draw(ctx) {
-        let random = 1;
         const initialTime = Date.now();
         for(let y = 0; y < this.camera.screenDimensions.height /2; y++) {
             for(let x = 0; x < this.camera.screenDimensions.width/2; x++) {
-                random = RandomNumberGenerator.XorShift(random);
+                const seed = 1;
+
+                const random = RandomNumberGenerator.XorShift(seed);
                 const color = random % 256;
                 ctx.fillStyle = `rgb(${color}, ${color}, ${color})`;
                 ctx.fillRect(x*2, y*2, 2, 2);
