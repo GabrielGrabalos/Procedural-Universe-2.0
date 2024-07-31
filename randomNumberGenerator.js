ffc82994a69f0751c41c8be9ed047fefbe792234
Gabriel Grabalos <bielgraba@gmail.com>
2024-07-31 18:15:29 -0300

    bigger pixels and time measuring

--- randomNumberGenerator.js
+++ randomNumberGenerator.js
@@ -13,13 +13,16 @@ class RandomNumberGenerator {
 
     draw(ctx) {
         let random = 1;
-        for(let y = 0; y < this.camera.screenDimensions.height; y++) {
-            for(let x = 0; x < this.camera.screenDimensions.width; x++) {
+        const initialTime = Date.now();
+        for(let y = 0; y < this.camera.screenDimensions.height /2; y++) {
+            for(let x = 0; x < this.camera.screenDimensions.width/2; x++) {
                 random = RandomNumberGenerator.XorShift(random);
                 const color = random % 256;
                 ctx.fillStyle = `rgb(${color}, ${color}, ${color})`;
-                ctx.fillRect(x, y, 1, 1);
+                ctx.fillRect(x*2, y*2, 2, 2);
             }
         }
+
+        console.log(`Time taken: ${Date.now() - initialTime}ms`);
     }
 }
