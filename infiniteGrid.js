c7569f5844cb1bf7fb6a5620b0b1a4c0afc2842c
Gabriel Grabalos <bielgraba@gmail.com>
2024-07-31 17:14:42 -0300

    we're getting there (inifinite grid)

--- infiniteGrid.js
+++ infiniteGrid.js
@@ -19,13 +19,11 @@ class InfiniteGrid {
         const stw0 = this.camera.ScreenToWorld({ x: 0, y: 0 });
 
         for (let i = stw0.x - this.camera.offset.x % interval; i <= this.camera.screenDimensions.width; i += interval) {
-            const wtsix = this.camera.WorldToScreen({ x: i, y: 0 }).x;
-            this.drawLine(ctx, wtsix, 0, wtsix, this.camera.screenDimensions.height, color, 1);
+            this.drawLine(ctx, i, 0, i, this.camera.screenDimensions.height, color, 1);
         }
     
         for (let i = stw0.y - this.camera.offset.y % interval; i <= this.camera.screenDimensions.height; i += interval) {
-            const wtsiy = this.camera.WorldToScreen({ x: 0, y: i }).y;
-            this.drawLine(ctx, 0, wtsiy, this.camera.screenDimensions.width, wtsiy, color, 1);
+            this.drawLine(ctx, 0, i, this.camera.screenDimensions.width, i, color, 1);
         }
     }
 }
