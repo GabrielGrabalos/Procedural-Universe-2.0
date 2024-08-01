31e2827b79118295d4b65d0a8b0c7a864daa49ed
Gabriel Grabalos <bielgraba@gmail.com>
2024-08-01 14:19:32 -0300

    mirror universe

--- universe.js
+++ universe.js
@@ -5,22 +5,22 @@ class Universe {
 
     draw(ctx) {
         const interval = 50;
-        
+
         const stw0 = this.camera.ScreenToWorld({ x: 0, y: 0 });
         const stw1 = this.camera.ScreenToWorld({ x: this.camera.screenDimensions.width, y: this.camera.screenDimensions.height });
-        
+
         ctx.fillStyle = '#000000';
         ctx.fillRect(stw0.x, stw0.y, stw1.x - stw0.x, stw1.y - stw0.y);
 
-        const initialX = stw0.x - this.camera.offset.x % interval;
-        const initialY = stw0.y - this.camera.offset.y % interval;
+        const initalX = Math.floor(stw0.x / interval) * interval;
+        const initalY = Math.floor(stw0.y / interval) * interval;
 
-        for (let i = initialX; i <= stw1.x; i += interval) {
-            for (let j = initialY; j <= stw1.y; j += interval) {
-                const seed = (i & 0xFFFF) << 16 | (j & 0xFFFF);
+        for (let y = initalY; y < stw1.y; y += interval) {
+            for (let x = initalX; x < stw1.x; x += interval) {
+                const seed = y**2 << 16 | x**2;
 
-                if (RandomNumberGenerator.randInt(seed, 0, 20) === 1) {
-                    const star = new Star(i, j);
+                if (RandomNumberGenerator.randInt(seed, 0, 20) == 1) {
+                    const star = new Star(x, y);
                     star.draw(ctx);
                 }
             }
