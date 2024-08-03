6cf5be2a55caa560cd415083583d0101dd3063af
Gabriel Grabalos <bielgraba@gmail.com>
2024-08-03 16:02:36 -0300

    Name generator fixed

--- nameGenerator.js
+++ nameGenerator.js
@@ -1,6 +1,7 @@
 class NameGenerator {
-    static alphabet = "abcdefghijklmnopqrstuvwxyz";
+    static alphabet = "abcdefghijlmnopqrstuvz";
     static vowels = "aeiou";
+    static goodConsonants = "bcdfgjlmnpqrstvz";
 
     static prefixes = [
         "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
@@ -8,7 +9,7 @@ class NameGenerator {
         "br", "cr", "dr", "fr", "gr", "pr", "tr",
         "fl", "gl", "pl",
         "qu",
-        "ch", "th"
+        "ch"
     ]
 
     static suffixes = [
@@ -18,6 +19,18 @@ class NameGenerator {
     static endings = [
         "s", "l"
     ]
+
+    static allowedLettersAfterLetter = {
+        "a": NameGenerator.goodConsonants + "eiou",
+        "e": NameGenerator.goodConsonants + "aiou",
+        "i": NameGenerator.goodConsonants + "aeou",
+        "o": NameGenerator.goodConsonants + "aeiu",
+        "u": NameGenerator.goodConsonants + "aeio",
+        "l": "bcdfgpqtv",
+        "n": "bcdfgpt",
+        "r": "bcdfgptv",
+        "s": "bcdfgklmnprtvz",
+    }
 
     static generateSilable(rng, previousSilable) {
         /**
@@ -34,31 +47,70 @@ class NameGenerator {
          *      2. Vowel: 100%
          *      3. Suffix: 40%
          *      4. Ending: 30% (if step 3 is a vowel)
+         * 
+         * Rules:
+         * 
+         *     - If the previous silable ended with a vowel, the current silable must start with a consonant.
+         *     - If the previous silable ended with a consonant, the current silable has start with a vowel.
         **/
 
         let silable = "";
 
-        // Step 1: Prefix
-        if (rng.nextInt(0, 100) < 70) {
-            silable += NameGenerator.prefixes[rng.nextInt(0, NameGenerator.prefixes.length - 1)];
+        let lastLetter;
+        let allowedLetters;
+
+        if (previousSilable) {
+            lastLetter = previousSilable[previousSilable.length - 1];
+            allowedLetters = NameGenerator.allowedLettersAfterLetter[lastLetter];
         }
 
-        // Step 2: Vowel
-        // Can't be a double vowel (would happen when "qu" is the prefix)
-        if (silable !== "qu") {
-            silable += NameGenerator.vowels[rng.nextInt(0, NameGenerator.vowels.length - 1)];
+        // 1. Prefix
+        // If the previous silable exists, we see what letter it ended with.
+        // We use the lookup table to see what letters can come after that letter.
+
+        if (rng.next() % 100 < 70 || (previousSilable && !NameGenerator.vowels.includes(lastLetter))) {
+            if (!previousSilable) {
+                silable += NameGenerator.prefixes[rng.nextInt(0, NameGenerator.prefixes.length - 1)];
+            }
+            else {
+                // Add a prefix that starts with an allowed letter.
+                let allowedPrefixes = NameGenerator.prefixes.filter(prefix => allowedLetters.includes(prefix[0]));
+
+                // Not allow 3 consecutive consonants.
+                if (NameGenerator.goodConsonants.includes(lastLetter)) {
+                    // Only allow single consonants or digraphs that start with a consonant.
+                    allowedPrefixes = allowedPrefixes.filter(prefix => prefix.length === 1 || prefix === "qu");
+                }
+
+                silable += allowedPrefixes[rng.nextInt(0, allowedPrefixes.length - 1)];
+            }
         }
-        else {
+
+        // 2. Vowel
+        if (previousSilable && silable === "") {
+            // Here means that the las letter was a vowel and no prefix was added.
+            // This means we need to add an allowed vowel.
+            // Which is just a different vowel from the last one.
+            let vowel = NameGenerator.vowels[rng.nextInt(0, NameGenerator.vowels.length - 1)];
+            while (vowel === lastLetter) {
+                vowel = NameGenerator.vowels[rng.nextInt(0, NameGenerator.vowels.length - 1)];
+            }
+
+            silable += vowel;
+        }
+        else if (silable === "qu") {
             silable += "aeio"[rng.nextInt(0, 3)];
         }
+        else {
+            silable += NameGenerator.vowels[rng.nextInt(0, NameGenerator.vowels.length - 1)];
+        }
 
-        // Step 3: Suffix
-        if (rng.nextInt(0, 100) < 20) {
-            silable += NameGenerator.suffixes[rng.nextInt(0, NameGenerator.suffixes.length - 1)];
+        // 3. Suffix
+        if (rng.next() % 100 < 40) {
+            let sufix = NameGenerator.suffixes[rng.nextInt(0, NameGenerator.suffixes.length - 1)];
 
-            // Step 4: Ending
-            if (NameGenerator.vowels.includes(silable[silable.length - 1]) && rng.nextInt(0, 100) < 30) {
-                silable += NameGenerator.endings[rng.nextInt(0, NameGenerator.endings.length - 1)];
+            while (silable.includes(sufix)) {
+                sufix = NameGenerator.suffixes[rng.nextInt(0, NameGenerator.suffixes.length - 1)];
             }
         }
 
@@ -68,8 +120,12 @@ class NameGenerator {
     static generateName(rng, silableCount) {
         let name = "";
 
+        let previousSilable;
+
         for (let i = 0; i < silableCount; i++) {
-            name += NameGenerator.generateSilable(rng);
+            const silable = NameGenerator.generateSilable(rng, previousSilable);
+            name += silable;
+            previousSilable = silable;
         }
 
         return name[0].toUpperCase() + name.slice(1);
