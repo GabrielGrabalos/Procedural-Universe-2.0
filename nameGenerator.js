82b494029dda22cbd32c7aaacb59de62db590d8f
Gabriel Grabalos <bielgraba@gmail.com>
2024-08-03 15:11:51 -0300

    Name generator update

--- nameGenerator.js
+++ nameGenerator.js
@@ -19,7 +19,7 @@ class NameGenerator {
         "s", "l"
     ]
 
-    static generateSilable(rng) {
+    static generateSilable(rng, previousSilable) {
         /**
          * Silables are going to be composed of 3 parts:
          * 
@@ -30,28 +30,36 @@ class NameGenerator {
          * 
          * The probability of each part is going to be:
          * 
-         *      1. Prefix: 40%
+         *      1. Prefix: 70%
          *      2. Vowel: 100%
-         *      3. Suffix: 60%
-         *      4. Ending: 40% (if step 3 is a vowel)
+         *      3. Suffix: 40%
+         *      4. Ending: 30% (if step 3 is a vowel)
         **/
 
         let silable = "";
 
         // Step 1: Prefix
-        if (rng.nextInt(0, 100) < 40) {
+        if (rng.nextInt(0, 100) < 70) {
             silable += NameGenerator.prefixes[rng.nextInt(0, NameGenerator.prefixes.length - 1)];
         }
 
         // Step 2: Vowel
         // Can't be a double vowel (would happen when "qu" is the prefix)
-        if (silable.length === 0 || !NameGenerator.vowels.includes(silable[silable.length - 1])) {
+        if (silable !== "qu") {
             silable += NameGenerator.vowels[rng.nextInt(0, NameGenerator.vowels.length - 1)];
+        }
+        else {
+            silable += "aeio"[rng.nextInt(0, 3)];
         }
 
         // Step 3: Suffix
-        if (rng.nextInt(0, 100) < 60) {
+        if (rng.nextInt(0, 100) < 20) {
             silable += NameGenerator.suffixes[rng.nextInt(0, NameGenerator.suffixes.length - 1)];
+
+            // Step 4: Ending
+            if (NameGenerator.vowels.includes(silable[silable.length - 1]) && rng.nextInt(0, 100) < 30) {
+                silable += NameGenerator.endings[rng.nextInt(0, NameGenerator.endings.length - 1)];
+            }
         }
 
         return silable;
